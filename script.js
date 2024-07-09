78ddd8c27a172f714c1dd60b69c5d28a404687e4
Kramik <kramikkafle7@gmail.com>
2024-07-09 15:12:16 +0545

    first commit

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const getWordInfo = async (word) => {
     const response = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     const data = await response.json();
 
-    resultDiv.innerHTML = `<h2>${data[0].meanings[0].definitions[0].definition}</h2>`;
+    resultDiv.innerHTML = `
+    <h2><strong>Word:</strong> ${data[0].word}</h2>
+    <p>${data[0].meanings[0].partOfSpeech}</p>
+    <p><strong>Meaning:</strong>${data[0].meanings[0].definitions[0].definition}</p>`;
     console.log(data);
 }
